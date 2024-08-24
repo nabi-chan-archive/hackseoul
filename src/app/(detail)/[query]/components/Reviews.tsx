2ac8d9d6918e1701c4ecafdfe01b0970b2daee22
nabi.lee <hello@nabi.kim>
2024-08-25 03:09:14 +0900

    hotfix: 리뷰가 불러와지지 않는 문제를 해결

--- src/app/(detail)/[query]/components/Reviews.tsx
+++ src/app/(detail)/[query]/components/Reviews.tsx
@@ -1,6 +1,8 @@
+import { setTimeout } from 'timers/promises'
 import { supabase } from '@/utils/supabase/client'
 import { Rating } from '@/components/rating'
 import { Card } from '@/components/card'
+import { http } from '@/utils/http'
 
 export default async function Reviews({ query }: { query: string }) {
   const products = await supabase
@@ -11,7 +13,7 @@ export default async function Reviews({ query }: { query: string }) {
     .then((response) => response.data ?? [])
     .then((products) => products.map((product) => product.id as string))
 
-  const reviews = await Promise.all(
+  let reviews = await Promise.all(
     Array.from({ length: Math.round(products.length / 25) }, (_, index) =>
       supabase
         .from('reviews')
@@ -21,6 +23,34 @@ export default async function Reviews({ query }: { query: string }) {
         .then((response) => response.data ?? [])
     )
   ).then((responses) => responses.flat())
+
+  if (reviews.length === 0) {
+    await Promise.all(
+      products.slice(0, 25).map(async (productId) => {
+        await setTimeout(1000)
+        return http.get('/api/crawl/reviews', {
+          productId,
+          page: 1,
+        })
+      })
+    )
+
+    await Promise.all(
+      products.map(async (productId) => {
+        await setTimeout(1000)
+        return await supabase
+          .from('reviews')
+          .select('*')
+          .eq('product_id', productId)
+          .throwOnError()
+          .then((response) => response.data ?? [])
+      })
+    ).then((responses) => {
+      reviews = responses.flat()
+    })
+  }
+
+  console.log('reviews length', reviews?.length, query)
 
   return (
     <div className="p-4 flex flex-col gap-4">
